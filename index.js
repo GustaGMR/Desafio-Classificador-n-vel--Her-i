164d9e31e2dcdec6d7913a21ba7858d96561e85b
GustaGMR <gustavo.grosa@gmail.com>
2026-02-27 18:42:32 -0300

    git push -u main

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 let nome = "";
-let nivel = "";
+let nivel = 0;
 
-function nivel {
+function calcularNivel(nivel) {
     if (nivel < 1000){
-        return "ferro"
+        return "ferro";
     } else if (nivel > 1001 && nivel < 2000 ){
-        return "Bronze"
+        return "Bronze";
     }else if (nivel > 2001 && nivel < 5000 ){
-        return "Prata"
+        return "Prata";
     }else if (nivel > 5001 && nivel < 7000 ){
-        return "Ouro"
+        return "Ouro";
     }else if (nivel > 7001 && nivel < 8000 ){
-        return "Platina"
+        return "Platina";
     }else if (nivel > 8001 && nivel < 9000 ){
-        return "Ascendente"
+        return "Ascendente";
     }else if (nivel > 9001 && nivel < 10000 ){
-        return "Imortal"
-    }else (nivel > 1001 && nivel < 2000 ){
-        return "Radiante"
+        return "Imortal";
+    }else{
+        return "Radiante";
     }
 }
+
+nome = "gustavo"
+nivel = 300
+
+console.log(`O Herói de nome ${nome} está no nível de ${calcularNivel(xp)}`)
