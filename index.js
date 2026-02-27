dda29404d008384f4c2d2ec6ec29d6f9efc22c19
GustaGMR <gustavo.grosa@gmail.com>
2026-02-27 18:34:01 -0300

    git branch -M main

---
+++ index.js
@@ -0,0 +1,22 @@
+let nome = "";
+let nivel = "";
+
+function nivel {
+    if (nivel < 1000){
+        return "ferro"
+    } else if (nivel > 1001 && nivel < 2000 ){
+        return "Bronze"
+    }else if (nivel > 2001 && nivel < 5000 ){
+        return "Prata"
+    }else if (nivel > 5001 && nivel < 7000 ){
+        return "Ouro"
+    }else if (nivel > 7001 && nivel < 8000 ){
+        return "Platina"
+    }else if (nivel > 8001 && nivel < 9000 ){
+        return "Ascendente"
+    }else if (nivel > 9001 && nivel < 10000 ){
+        return "Imortal"
+    }else (nivel > 1001 && nivel < 2000 ){
+        return "Radiante"
+    }
+}
